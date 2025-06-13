837ab670df39256bc7b6e1ab2f488f0c9d260f8b
Sandesh Budiyal <sandesh.bdyl.2060@gmail.com>
2025-06-13 08:13:24 +0545

    Added some scroll animation & loader

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const Loader = document.querySelector('.loader');
+    const Spinner = document.querySelector('spinner');
+
+    Loader.style.display = "none";
+    Spinner.style.display = "none";
+})
+
 const MenuTitle = document.getElementById("menu-title");
 const MenuIcon = document.getElementById("menu-icon");
 const DropDownContainer = document.getElementById("drop-down-menu");
@@ -8,7 +16,7 @@ MenuTitle.addEventListener(("click"), (e) => {
     e.target.style.display ="none";
     DropDownContainer.style.display = "flex";
     MenuIcon.style.translate = "-95px 0px";
-    MenuIcon.style.animation= "fadeOut 0.5s 1 linear";
+    MenuIcon.style.animation= "fadeOut 0.5s infinite linear";
     setTimeout(() => {MenuIcon.style.display = "none";
                       CrossIcon.style.display = "flex";
     }, 500);
@@ -30,4 +38,5 @@ CrossIcon.addEventListener(("click"), (e) => {
     MenuTitle.style.display = "flex"
     MenuIcon.style.translate = "0px 0px";
     MenuIcon.style.display = "flex";
+    MenuIcon.style.animation= "none"; 
 });
